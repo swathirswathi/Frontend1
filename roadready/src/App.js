fe0da984a650d6505aa85b95d2c77a69050a0201
Swathi R <swathirm27@gmail.com>
2024-02-18 17:38:26 +0530

    initial

---
+++ roadready/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import RegisterUser from './Components/RegisterUser/RegisterUser';
+import RegisterAdmin from './Components/RegisterAdmin/RegisterAdmin';
+import './App.css';
+import Login from './Components/Login/Login';
+import Ride from './Components/Ride/Ride';
+import Review from './Components/Review/Review';
+import AboutUs from './Components/AboutUs/AboutUs';
+import Footer from './Components/Footer/Footer';
+import CarServices from './Components/CarServices/CarServices';
+import Reservation from './Components/Reservation/Reservation';
+
+function App() {
+  return (
+    //<RegisterUser/>
+    //<RegisterAdmin/>
+    //<Login/>
+    //<Ride/>
+    //<Review/>
+    //<AboutUs/>
+    //<Footer/>
+    <CarServices/>
+    // <Reservation/>
+  );
+}
+
+export default App;
